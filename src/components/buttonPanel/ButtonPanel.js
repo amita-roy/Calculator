1ff4297dd70698947bdb4898ad080354540acb3b
Amita-Roy <sardana.amita2@gmail.com>
2021-03-16 16:49:00 +0100

    Add all the required button to buttonPanel

--- src/components/buttonPanel/ButtonPanel.js
+++ src/components/buttonPanel/ButtonPanel.js
@@ -6,7 +6,35 @@ import './ButtonPanel.css';
 export default function ButtonPanel() {
   return (
     <div className="btn-panel">
-      <Button name="" />
+      <div className="row">
+        <Button name="AC" />
+        <Button name="+/-" />
+        <Button name="%" />
+        <Button name="➗" />
+      </div>
+      <div className="row">
+        <Button name="7" />
+        <Button name="8" />
+        <Button name="9" />
+        <Button name="X" />
+      </div>
+      <div className="row">
+        <Button name="4" />
+        <Button name="5" />
+        <Button name="6" />
+        <Button name="-" />
+      </div>
+      <div className="row">
+        <Button name="1" />
+        <Button name="2" />
+        <Button name="3" />
+        <Button name="+" />
+      </div>
+      <div className="row">
+        <Button name="0" />
+        <Button name="." />
+        <Button name="=" />
+      </div>
     </div>
   );
 }
