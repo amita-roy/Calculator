ddfc42d8ee3c68589f3ff9e6b169dfa4287e02bb
Amita-Roy <sardana.amita2@gmail.com>
2021-03-16 15:33:25 +0100

    Add buttonPanel js and css file

---
+++ src/components/buttonPanel/ButtonPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../button/Button';
+
+import './ButtonPanel.css';
+
+export default function ButtonPanel() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
