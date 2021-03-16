7b5fe963836b2229a47420c4464bfeeacaad7528
Amita-Roy <sardana.amita2@gmail.com>
2021-03-16 15:31:33 +0100

    Add a app folder in components. Add App.js and App.css file

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ButtonPanel from '../buttonPanel/ButtonPanel';
+import Display from '../display/Display';
+
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
+}
+
+export default App;
