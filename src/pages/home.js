7a8e8ccb4df2c11bb897e3c1668bbde413acccee
Amita-Roy <sardana.amita2@gmail.com>
2021-03-25 21:57:45 +0100

    Navigation style correction

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './home.css';
+import Illustration from '../assets/images/undraw_mathematics_4otb.svg';
 
 const Home = () => (
   <div className="home">
-    <h1>Home</h1>
+    <div className="home-content">
+      <p>LOrem</p>
+    </div>
+    <div className="illustration">
+      <img src={Illustration} alt="illustration" />
+    </div>
   </div>
 );
 
