566f099f349b53630dc8b7f6627f6f1a6cc97980
Amita-Roy <sardana.amita2@gmail.com>
2021-03-25 22:17:24 +0100

    Add content fo homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,12 @@ import Illustration from '../assets/images/undraw_mathematics_4otb.svg';
 const Home = () => (
   <div className="home">
     <div className="home-content">
-      <p>LOrem</p>
+      <h3 className="tagline">Math and Success Add Up</h3>
+      <p className="left-text">
+        Mathematics is on the artistic side a creation of new rhythms,
+        orders, designs, harmonies, and on the knowledge side, is a systematic
+        study of various rhythms, orders.
+      </p>
     </div>
     <div className="illustration">
       <img src={Illustration} alt="illustration" />
