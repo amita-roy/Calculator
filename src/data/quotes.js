11d12674360584172a63d00ba73ba1bbe9bcbcb6
Amita-Roy <sardana.amita2@gmail.com>
2021-03-25 21:10:27 +0100

    Add quotes

---
+++ src/data/quotes.js
@@ -0,0 +1,104 @@
+const QUOTES = [
+  {
+    quote: 'Mathematics is a game played according to certain rules with meaningless marks on paper.',
+    author: 'David Hilbert',
+  },
+  {
+    quote: 'Mathematics is concerned only with the enumeration and comparison of relations.',
+    author: 'Friedrich Gauss',
+  },
+  {
+    quote: 'Mathematics is the door and key to the sciences.',
+    author: 'Roger Bacon',
+  },
+  {
+    quote: 'Mathematics is the science of what is clear by itself.',
+    author: 'Carl Jacobi',
+  },
+  {
+    quote: 'Mathematics – the unshaken Foundation of Sciences, and the plentiful Fountain of Advantage to human affairs.',
+    author: 'Isaac Barrow',
+  },
+  {
+    quote: 'Mathematics is as much an aspect of culture as it is a collection of algorithms.',
+    author: 'Carl Boyer',
+  },
+  {
+    quote: 'Mathematics is the art of giving the same name to different things.',
+    author: 'Henri Poincaré',
+  },
+  {
+    quote: 'Mathematics is not a careful march down a well-cleared highway, but a journey into a strange wilderness, where the explorers often get lost. Rigour should be a signal to the historian that the maps have been made, and the real explorers have gone elsewhere.',
+    author: 'W. S. A',
+  },
+  {
+    quote: 'Mathematics, as much as music or any other art, is one of the means by which we rise to a complete self-consciousness. The significance of mathematics resides precisely in the fact that it is an art; by informing us of the nature of our own minds it informs us of much that depends on our minds.',
+    author: 'John William Navin Sullivan',
+  },
+  {
+    quote: 'Mathematics is the supreme judge; from its decisions there is no appeal.',
+    author: 'Tobias Dantzig',
+  },
+  {
+    quote: 'Mathematics is no more computation than typing is literature.',
+    author: 'John Allen Paulos',
+  },
+  {
+    quote: 'Mathematics is the language with which God wrote the universe.',
+    author: 'Galileo',
+  },
+  {
+    quote: 'Mathematics is the handwriting on the human consciousness of the very spirit of life itself.',
+    author: 'Claude Bragdon',
+  },
+  {
+    quote: ' Mathematics is the queen of science.',
+    author: 'Carl Friedrich Gauss',
+  },
+  {
+    quote: 'Mathematics may be defined as the subject in which we never know what we are talking about, nor whether what we are saying is true.',
+    author: 'Bertrand Russell',
+  },
+  {
+    quote: 'Mathematics consists in proving the most obvious thing in the least obvious way.',
+    author: 'George Polya',
+  },
+  {
+    quote: 'Mathematics is like love; a simple idea, but it can get complicated.',
+    author: 'Unknown',
+  },
+  {
+    quote: 'Mathematics is like checkers in being suitable for the young, not too difficult, amusing, and without peril to the state.',
+    author: 'Plato',
+  },
+  {
+    quote: 'Mathematics is not a deductive science – that’s a cliché. When you try to prove a theorem, you don’t just list the hypotheses, and then start to reason. What you do is trial and error, experimentation, guesswork.',
+    author: 'Paul Halmos',
+  },
+  {
+    quote: 'Mathematics is written for mathematicians.',
+    author: 'Copernicus',
+  },
+  {
+    quote: 'Mathematics is, I believe, the chief source of the belief in eternal and exact truth, as well as a sensible intelligible world.',
+    author: 'Bertrand Russell',
+  },
+  {
+    quote: 'Mathematics is the tool specially suited for dealing with abstract concepts of any kind and there is no limit to its power in this field.',
+    author: 'P. Dirac',
+  },
+  {
+    quote: 'Mathematics is not only real, but it is the only reality.',
+    author: 'Martin Gardner',
+  },
+  {
+    quote: 'Mathematics is the door and key to the sciences.',
+    author: 'Roger Bacon',
+  },
+  {
+    quote: 'Mathematics is often erroneously referred to as the science of common sense.',
+    author: 'Newman & Kasner',
+  },
+];
+
+export default QUOTES;
