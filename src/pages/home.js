13cabb920bce2aadd4579fb2bbad192870ddf2b5
Amita-Roy <sardana.amita2@gmail.com>
2021-03-26 08:23:07 +0100

    Update image of homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './home.css';
-import Illustration from '../assets/images/undraw_mathematics_4otb.svg';
+// import Illustration from '../assets/images/undraw_mathematics_4otb.svg';
+import Illustration from '../assets/images/undraw_mathematics_4otb2.svg';
 
 const Home = () => (
   <div className="home">
     <div className="home-content">
-      <h3 className="tagline">Math and Success Add Up</h3>
+      <h3 className="tagline">MATH AND SUCCESS ADD UP</h3>
       <p className="left-text">
         Mathematics is on the artistic side a creation of new rhythms,
         orders, designs, harmonies, and on the knowledge side, is a systematic
