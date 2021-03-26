f0fddfeb21b5caa4207a51b2c476f05c9d88341d
Amita-Roy <sardana.amita2@gmail.com>
2021-03-26 09:59:49 +0100

    Update calculator page

--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Calculator from '../components/calculator/Calculator';
+import './calculator-page.css';
 
-const Calculator = () => (
-  <div>
-    <h1>Calculator</h1>
+const CalculatorPage = () => (
+  <div className="calculator-page">
+    <Calculator />
+    <p className="bottom-text">
+      Mathematics is a hard thing to love. It has the unfortunate habit,
+      like a rude dog, of turning its most unfavourable side towards you when you first
+      make contact with it.
+    </p>
   </div>
 );
 
-export default Calculator;
+export default CalculatorPage;
