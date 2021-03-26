455dec4d5d792b11410d7424d64f0579903bc021
Amita-Roy <sardana.amita2@gmail.com>
2021-03-26 10:50:09 +0100

    Update style for app

--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -5,11 +5,6 @@ import './calculator-page.css';
 const CalculatorPage = () => (
   <div className="calculator-page">
     <Calculator />
-    <p className="bottom-text">
-      Mathematics is a hard thing to love. It has the unfortunate habit,
-      like a rude dog, of turning its most unfavourable side towards you when you first
-      make contact with it.
-    </p>
   </div>
 );
 
